1f8b8b5e499d7c95638019e4c91fe76db51babcb
Jonathan Hellwig <jonathan.hellwig@posteo.de>
2022-06-12 21:59:36 +0200

    Add sine test case

--- experiments/sde.py
+++ experiments/sde.py
@@ -139,6 +139,15 @@ def batches(x, y, batch_size, key):
                jnp.array_split(y[permuated_indices], number_of_splits))
 
 
+def sample_batch(key, x, y, batch_size):
+    key, key_x, key_y = random.split(key, 3)
+    return random.choice(key_x, x,
+                         shape=(batch_size,
+                                1)), random.choice(key_y,
+                                                   y,
+                                                   shape=(batch_size, 1))
+
+
 # Goals
 # 1. Write test cases
 # 2. Adjust the iterations of each run such that they can be compared
@@ -149,6 +158,77 @@ def linear_run():
 
     key = random.PRNGKey(1)
     sizes = [1, 1]
+    sde_parameters = init_network_parameters(sizes, key)
+    gd_parameters = sde_parameters.copy()
+    sgd_parameters = sde_parameters.copy()
+    svag_parameters = sde_parameters.copy()
+    step_size = 0.001
+    final_time = 5.0
+    t = jnp.arange(0.0, final_time, step_size)
+    learning_rate = 0.1
+    batch_size = 32
+    svag_l = 4
+    solver_iterations = 10
+    gd_losses = []
+    sgd_losses = []
+    sde_losses = []
+    svag_losses = []
+    for i in range(len(t[::solver_iterations])):
+        for _ in range(solver_iterations):
+            key, subkey = random.split(key)
+            sde_parameters = sde_update(sde_parameters, x, y, step_size,
+                                        learning_rate, subkey)
+            sde_losses.append(loss(sde_parameters, x, y))
+
+        # sampling with replacement
+        key, subkey = random.split(key)
+        sgd_x_batch, sgd_y_batch = sample_batch(subkey, x, y, batch_size)
+        sgd_parameters = gd_update(sgd_parameters, sgd_x_batch, sgd_y_batch,
+                                   learning_rate)
+        sgd_losses.append(loss(sgd_parameters, x, y))
+
+        key, subkey = random.split(key)
+        svag_x_batch_first, svag_y_batch_first = sample_batch(subkey, x, y, batch_size)
+        key, subkey = random.split(key)
+        svag_x_batch_second, svag_y_batch_second = sample_batch(subkey, x, y, batch_size)
+        svag_parameters = svag_update(svag_parameters, svag_x_batch_first,
+                                      svag_y_batch_first, svag_x_batch_second,
+                                      svag_y_batch_second, svag_l)
+        svag_losses.append(loss(svag_parameters, x, y))
+
+        gd_parameters = gd_update(gd_parameters, x, y, learning_rate)
+        gd_losses.append(loss(gd_parameters, x, y))
+        print(f'sgd t: {t[i]}, loss: {sgd_losses[-1]}')
+        print(f'sde: {t[i]}, loss: {sde_losses[-1]}')
+        print(f'gd t: {t[i]}, loss: {gd_losses[-1]}')
+
+    y_hat_sde = batched_predict(sde_parameters, x)
+    y_hat_gd = batched_predict(gd_parameters, x)
+    y_hat_sgd = batched_predict(sgd_parameters, x)
+    y_hat_svag = batched_predict(svag_parameters, x)
+    plt.figure()
+    plt.plot(x, y_hat_sde, label="sde")
+    plt.plot(x, y_hat_gd, label="gd")
+    plt.plot(x, y_hat_sgd, label="sgd")
+    plt.plot(x, y_hat_svag, label="svag")
+    plt.plot(x, y, label="ground truth")
+    plt.legend()
+
+    plt.figure()
+    plt.plot(t, sde_losses, label="sde")
+    plt.plot(t[::solver_iterations], gd_losses, label="gd")
+    plt.plot(t[::solver_iterations], sgd_losses, label="sgd")
+    plt.plot(t[::solver_iterations], svag_losses, label="svag")
+    plt.legend()
+    plt.show()
+
+
+def sin_test_case():
+    x = jnp.linspace(0.0, 2.0, 100).reshape((100, 1))
+    y = jnp.sin(x)
+
+    key = random.PRNGKey(1)
+    sizes = [1, 128, 1]
     sde_parameters = init_network_parameters(sizes, key)
     gd_parameters = sde_parameters.copy()
     sgd_parameters = sde_parameters.copy()
